5f0396dfe7d3d4351a13501dd9a343b3051b044d
Jeremias91 <126523676+Jeremias91@users.noreply.github.com>
2023-11-20 17:36:15 -0300

    Add files via upload

---
+++ Tienda De Ropa UML/Remeras.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class Remeras
+{
+
+  #region Aggregations
+
+
+  #endregion
+
+  #region Compositions
+
+
+  #endregion
+
+  #region Attributes
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public string Marca
+  {
+    get
+    {
+      return m_Marca;
+    }
+    set
+    {
+      m_Marca = value;
+    }
+  }
+  private string m_Marca;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public int Talle
+  {
+    get
+    {
+      return m_Talle;
+    }
+    set
+    {
+      m_Talle = value;
+    }
+  }
+  private int m_Talle;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public string Modelo
+  {
+    get
+    {
+      return m_Modelo;
+    }
+    set
+    {
+      m_Modelo = value;
+    }
+  }
+  private string m_Modelo;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public string Material
+  {
+    get
+    {
+      return m_Material;
+    }
+    set
+    {
+      m_Material = value;
+    }
+  }
+  private string m_Material;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public string Color
+  {
+    get
+    {
+      return m_Color;
+    }
+    set
+    {
+      m_Color = value;
+    }
+  }
+  private string m_Color;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private string Estilo;
+
+
+
+  #endregion
+
+
+  #region Public methods
+
+  #endregion
+
+
+  #region Protected methods
+
+  #endregion
+
+
+  #region Private methods
+
+  #endregion
+
+
+}
+
